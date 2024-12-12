8e37ce6e91d8036be5c90461c3bee2b7ff422d24
ZestyGlasses <zesty-glasses@protonmail.com>
2024-12-12 16:02:00 +0100

    Update GoogleMap.tsx

--- src/features/map/components/GoogleMap.tsx
+++ src/features/map/components/GoogleMap.tsx
@@ -3,7 +3,7 @@ interface MapLoader {
 }
 
 export const Map = ({ eventLocation }: MapLoader) => {
-    const apiKey = "AIzaSyB5p476Apq41dAgr2zCIlNue2RQCXSaXws";
+    const apiKey = "...";
     const mapSrc = `https://maps.googleapis.com/maps/api/staticmap?center=${eventLocation.lat},${eventLocation.lng}&zoom=12&size=600x300&markers=${eventLocation.lat},${eventLocation.lng}&key=${apiKey}`;
     const googleMapsUrl = `https://www.google.com/maps/?q=${eventLocation.lat},${eventLocation.lng}`;
 
